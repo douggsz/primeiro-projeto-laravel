e89c138052dc096af89c05c9cbad0a5fc744e11a
Douglas Barbosa <douglasbarbosa030@gmail.com>
2019-07-12 04:44:17 -0300

    adição de views e componentes

---
+++ resources/views/componentes/barraNavegacao.blade.php
@@ -0,0 +1,35 @@
+<nav class="navbar navbar-inverse navbar-fixed-top" >
+    <div class="container-fluid">
+        <!-- Brand and toggle get grouped for better mobile display -->
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle button-sm collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar">Menu</span>
+            </button>
+            <a class="navbar-brand" href="#">Cadastro de Itens</a>
+        </div>
+
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+            <ul class="nav navbar-nav">
+                <li class="active"><a href="#">Cadastrar <span class="sr-only">(current)</span></a></li>
+                <li><a href="#">Buscar</a></li>
+                <li class="dropdown">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Mais <span class="caret"></span></a>
+                    <ul class="dropdown-menu">
+                        <li><a href="#">Deletar</a></li>
+                        <li><a href="#">Ver Tudo</a></li>
+                        <li><a href="#">Excluir</a></li>
+                        <li role="separator" class="divider"></li>
+                    </ul>
+                </li>
+            </ul>
+            <form class="navbar-form navbar-left">
+                <div class="form-group">
+                    <input type="text" class="form-control" placeholder="Pesquisa">
+                </div>
+                <button type="submit" class="btn btn-default">Buscar</button>
+            </form>
+        </div><!-- /.navbar-collapse -->
+    </div><!-- /.container-fluid -->
+</nav>
---
+++ resources/views/inicio.blade.php
@@ -0,0 +1,8 @@
+@extends('layout.layout')
+
+@section('titulo','Cadastro')
+
+@section('barraSuperior')
+    @component('componentes.barraNavegacao');
+        @endcomponent
+    @endsection
